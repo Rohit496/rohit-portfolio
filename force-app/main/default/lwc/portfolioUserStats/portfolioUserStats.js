ef80f32d04e5ca779378bed2ac56624a3fb4f3a9
Rohit <rohitdotnet@outlook.com>
2024-06-25 15:46:57 +0800

    feat: Update user stats image and values
    Updated the user stats image and values in the PortfolioUserStats component to match the user's actual stats. This ensures that the displayed information accurately reflects the user's achievements.

--- force-app/main/default/lwc/portfolioUserStats/portfolioUserStats.js
+++ force-app/main/default/lwc/portfolioUserStats/portfolioUserStats.js
@@ -1,6 +1,7 @@
 import { LightningElement } from 'lwc';
 import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets';
 export default class PortfolioUserStats extends LightningElement {
+    // updated the values to match the user's stats
     thrailheadRankingImage = `${PortfolioAssets}/PortfolioAssets/Ranks/All star ranger.png`;
     badges = 777;
     points = 310062;
